7ebc5c64ce6d07de4e6bd73987782be86e8a71ab
Christopher Stock <christopher.stock@mayflower.de>
2022-09-06 10:35:20 +0200

    Enhance mayflower.de 'Stellenangebote' test

--- tests/mayflower.spec..ts
+++ tests/mayflower.spec..ts
@@ -1,20 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('Mayflower Homepage - Menu Link "Stellenangebote"', async ({ page }) => {
-    await page.goto('https://www.mayflower.de/');
 
-    // assert title
+    // open Mayflower homepage and assert title
+    await page.goto('https://www.mayflower.de/');
     await expect(page).toHaveTitle(/Agile Teams für moderne Softwareentwicklung - Mayflower GmbH/);
 
-    // get menu link 'Stellenangebote'
+    // get locator for menu link 'Stellenangebote' and assert href attribute
     const jobsLink = page.locator('text=Stellenangebote');
-
-    // expect href attribute exact value
     await expect(jobsLink).toHaveAttribute('href', 'https://mayflower.de/karriere/');
+    await expect(jobsLink).toBeVisible();
 
-    // click 'Stellenangebote' link
+    // click 'Stellenangebote' link and assert new URL and new title
     await jobsLink.click();
-
-    // expect URL to match
     await expect(page).toHaveURL(/.*\/karriere/);
+    await expect(page).toHaveTitle(/Aktuelle Stellenangebote bei Mayflower - Mayflower GmbH/);
+
+    // get locator on headline
+    const headline = page.locator('id=opening-party-1');
+    await expect(headline).toHaveText('The best gift is you');
 });
