a0491c8eb43aeb25359c89c877dd8e9d1612d681
Christopher Stock <christopher.stock@mayflower.de>
2022-09-06 10:59:37 +0200

    New group for tests.

--- tests/mayflower.spec..ts
+++ tests/mayflower.spec..ts
@@ -1,22 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('Mayflower Homepage - Menu Link "Stellenangebote"', async ({ page }) => {
+test.describe("Mayflower Homepage", () => {
+    test.beforeEach(async ({ page }) => {
+        // open Mayflower homepage
+        await page.goto('https://www.mayflower.de/');
+    });
 
-    // open Mayflower homepage and assert title
-    await page.goto('https://www.mayflower.de/');
-    await expect(page).toHaveTitle(/Agile Teams für moderne Softwareentwicklung - Mayflower GmbH/);
+    test('Mayflower Homepage - Menu Link "Stellenangebote"', async ({ page }) => {
 
-    // get locator for menu link 'Stellenangebote' and assert href attribute
-    const jobsLink = page.locator('text=Stellenangebote');
-    await expect(jobsLink).toHaveAttribute('href', 'https://mayflower.de/karriere/');
-    await expect(jobsLink).toBeVisible();
+        // assert page title
+        await expect(page).toHaveTitle(/Agile Teams für moderne Softwareentwicklung - Mayflower GmbH/);
 
-    // click 'Stellenangebote' link and assert new URL and new title
-    await jobsLink.click();
-    await expect(page).toHaveURL(/.*\/karriere/);
-    await expect(page).toHaveTitle(/Aktuelle Stellenangebote bei Mayflower - Mayflower GmbH/);
+        // get locator for menu link 'Stellenangebote' and assert href attribute
+        const jobsLink = page.locator('text=Stellenangebote');
+        await expect(jobsLink).toHaveAttribute('href', 'https://mayflower.de/karriere/');
+        await expect(jobsLink).toBeVisible();
 
-    // get locator on headline
-    const headline = page.locator('id=opening-party-1');
-    await expect(headline).toHaveText('The best gift is you');
+        // click 'Stellenangebote' link and assert new URL and new title
+        await jobsLink.click();
+        await expect(page).toHaveURL(/.*\/karriere/);
+        await expect(page).toHaveTitle(/Aktuelle Stellenangebote bei Mayflower - Mayflower GmbH/);
+
+        // get locator on headline
+        const headline = page.locator('id=opening-party-1');
+        await expect(headline).toHaveText('The best gift is you');
+        await expect(headline).toHaveCSS('font-weight', '700');
+    })
 });
